72be817085ab201958495bada6ec650aa78f22b6
Daniel Oropeza <jdanielosv@hotmail.com>
2020-10-03 10:08:52 -0400

    import modules

---
+++ jsx/src/index.js
@@ -0,0 +1,9 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a react component
+
+
+// Take the react component and show it on the screen
+
