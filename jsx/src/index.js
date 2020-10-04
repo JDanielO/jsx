9c7bddf1b90ef96ce3d874d2866fe2994022bcee
Daniel Oropeza <jdanielosv@hotmail.com>
2020-10-04 12:56:43 -0400

    Add fields

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,14 +1,24 @@
 // Import the React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>
+  const buttonText = { text: "Click Me" };
+  const labelText = "Enter name";
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
