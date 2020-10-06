9ab87f586fd23df2f9da806ba318a377b8250c96
Daniel Oropeza <jdanielosv@hotmail.com>
2020-10-06 11:39:48 -0400

    add conditionals

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -20,13 +20,13 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
-      </div>
-    );
+    if (this.state.errorMessageMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessageMessage && this.state.lat) {
+      return <div> Latitude: {this.state.lat}</div>;
+    }
+    return <div> Loading! </div>;
   }
 }
 
