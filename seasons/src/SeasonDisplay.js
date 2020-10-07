e2c2295243fa38ef3d2da4a7ad725422c43850dd
Daniel Oropeza <jdanielosv@hotmail.com>
2020-10-07 12:48:53 -0400

    display season using ternary expressions

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,9 +1,22 @@
 import React from "react";
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat < 0 ? "summer" : "winter";
+  }
+};
 const SeasonDisplay = (props) => {
-  console.log(prop.lat);
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
 
-  return <div> Season Display</div>;
+  return (
+    <div>
+      <h1>{text} </h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
