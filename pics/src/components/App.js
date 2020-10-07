8d1d96e7abb7d2c97867de682664f878f6f73ccc
Daniel Oropeza <jdanielosv@hotmail.com>
2020-10-07 19:48:59 -0400

    add form

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import SeachBar from "./SearchBar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <SeachBar />
+    </div>
+  );
 };
 
 export default App;
---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import react from "React";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
